1dd44fd7a13ce134c2d4d0a3c7e0f74584865950
mac <mac@MacBook-Pro-mac.local>
2024-02-16 23:00:09 +0200

    updated Verify hook

--- src/hooks/Verify.js
+++ src/hooks/Verify.js
@@ -23,7 +23,7 @@ const useScript = url => {
           }).then(res =>
             serverResponse.summaryResult === 'PASS'
               ? window.confirm('Automated ID Verification successful.') &&
-                window.location.replace(`${config.APP_URL}/oauth/auth?response_type=code&scope=signature&client_&redirect_uri=${config.APP}/signup`)
+                window.location.replace(`${config.APP_URL}/oauth/auth?response_type=code&scope=signature&client_&redirect_uri=${config.APP}`)
               : window.confirm('Automated ID Verification unsuccessful.'),
           );
         },
