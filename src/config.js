3ad572d4bf9897b63a244db75d8574901307443e
mac <mac@MacBook-Pro-mac.local>
2024-02-16 15:09:28 +0200

    changed app url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  API_URL: "http://18.197.102.150/api",
+  API_URL: "https://intheday.eu/api",
   VERIFY_URL: "https://verify.icomplykyc.com/icomply-min.js",
   APP_URL: 'http://localhost:3500',
-  APP: 'https://intheday.icomplykyc.com/np/main',
+  APP: 'https://intheday.eu/api',
 };
